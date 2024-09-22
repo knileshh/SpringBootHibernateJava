c0dc5c7ad8cc0d01207c3d67329061c19bdf2729
knileshh <neutron_stays@protonmail.com>
2024-09-22 18:56:50 +0530

    Init Working

---
+++ knshtest/src/main/java/com/knsh/knshtest/KnshtestApplication.java
@@ -0,0 +1,13 @@
+package com.knsh.knshtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnshtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnshtestApplication.class, args);
+	}
+
+}
---
+++ knshtest/src/test/java/com/knsh/knshtest/KnshtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knsh.knshtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnshtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
